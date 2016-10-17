3e81e741478d7bfa23b32634ac8725fc0051023f
Tom Ver Donck <verdonck_tom@hotmail.be>
2016-10-17 18:05:22 +0200

    Initialize express and body-parser Initialize HTTP server on port 8383 + verify with console message

--- JavaHTTPCalculator/main.js
+++ JavaHTTPCalculator/main.js
@@ -1,7 +1,8 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+var express = require('express');
+var bodyparser = require('body-parser');
+
+
+app.listen(8383);
+console.log("Server boot.");
 
