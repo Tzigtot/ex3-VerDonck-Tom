b55fa59dc417adf84c59e65867623a082aa35602
Tom Ver Donck <verdonck_tom@hotmail.be>
2016-10-17 18:10:48 +0200

    Use bodyparser to expose our keys and values. Display the HTML from the calculator using GET. (Huge thanks to Jelle Van Loock)

--- JavaHTTPCalculator/main.js
+++ JavaHTTPCalculator/main.js
@@ -2,6 +2,18 @@
 var express = require('express');
 var bodyparser = require('body-parser');
 
+var app = express();
+
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyparser.json());
+var result = [];
+
+app.get("/", function(request, response){
+    response.sendfile(__dirname + '/index.html');
+});
 
 app.listen(8383);
 console.log("Server boot.");
