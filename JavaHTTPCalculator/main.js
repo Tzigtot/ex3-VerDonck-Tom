ae86f6fa745c4d03f2d1fa1719fdc8fcc3516f9c
Tom Ver Donck <verdonck_tom@hotmail.be>
2016-10-18 13:25:32 +0200

    Add GET and POST methods to expose our calculator functions to the client, allowing them to view results and calculate values.

--- JavaHTTPCalculator/main.js
+++ JavaHTTPCalculator/main.js
@@ -15,6 +15,17 @@ app.get("/", function(request, response){
     response.sendfile(__dirname + '/index.html');
 });
 
-app.listen(8383);
-console.log("Server boot.");
+app.get("/results", function(request, response){
+  response.send(calculatedValue);
+});
 
+app.post("/", function(request, response){
+    var calculation = request.body.display;
+    var calculatedValue = eval(request.body.display);
+    uitkomst.push({"calculation": calculation, "result": calculatedValue});
+    console.log(calculatedValue);
+    response.status(200).send(calculatedValue);
+    });
+
+app.listen,(8383);
+console.log("Server boot.");
